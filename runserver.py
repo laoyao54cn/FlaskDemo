db2237d0f21aab1c668ce82408913619df050e06
longzxker <longzx123>
2018-08-07 21:20:41 +0800

    Add secret_key to file, and give session value in hello_world().like session['username']=

--- runserver.py
+++ runserver.py
@@ -1,10 +1,14 @@
 from News import app
+from flask import session
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    session['username'] = 'Aming'
+    return '<html><head><title>main page</title></head><body><a href="/News/adddata">添加新闻数据</a></body></html>'
 
+
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 if __name__ == '__main__':
     app.run(debug=True)
