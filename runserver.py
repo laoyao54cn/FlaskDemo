ce3e8a3968bb98a7ae79580de9c35f3fe77f4c98
longzxker <longzx123>
2018-08-07 21:23:28 +0800

    modify secret_key random

--- runserver.py
+++ runserver.py
@@ -1,6 +1,6 @@
 from News import app
 from flask import session
-
+import os
 
 @app.route('/')
 def hello_world():
@@ -8,7 +8,8 @@ def hello_world():
     return '<html><head><title>main page</title></head><body><a href="/News/adddata">添加新闻数据</a></body></html>'
 
 
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+# app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+app.secret_key = os.urandom(24)
 
 if __name__ == '__main__':
     app.run(debug=True)
